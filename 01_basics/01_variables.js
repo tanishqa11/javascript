1d8807923343dd7e8049d20ac6253bd4ee59121d
Tanishqa garg <101317556+tanishqa11@users.noreply.github.com>
2024-07-24 12:22:37 +0000

    3 types of variable 1.const- 2.let 3.var We can even directly use a varible but not recommended use table to print together

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=445   //cannot be changed
+let mail="tanishqa@mail.com"
+var password="1234"   //do not use var because of issue in block scope
+city="jaipur"         //
+//accountId=2       not allowed
+
+console.log(accountId)
+
+mail="koel@gmail.com"
+password='123456'
+city="delhi"
+let try1;    //undefined datatypes
+console.table([mail,city,password,try1]);
