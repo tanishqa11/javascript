728a8c5a28721df96bca46f9e275c1d3a23095c2
Tanishqa garg <101317556+tanishqa11@users.noreply.github.com>
2024-07-25 17:30:56 +0000

    8 datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,5 +4,12 @@ console.log(typeof false );
 console.log(typeof 1 );
 console.log(typeof "abs");
 console.log(typeof Symbol);
+console.log("trying.......");
+let score="33"
+let fault="31abc"
 
+let newscore=Number(score);
+let faultnew=Number(fault);
+console.log(typeof newscore);
+console.log( faultnew);  //gives  NaN
 
